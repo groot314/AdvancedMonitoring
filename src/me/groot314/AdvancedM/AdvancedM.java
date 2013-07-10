8622cf05abe0b5ce2ed2d0b45c87364cc780cd99
groot314 <groot314@gmail.com>
2013-07-09 22:27:50 -0700

    Added the basics for a bukkit plugin.

---
+++ src/me/groot314/AdvancedM/AdvancedM.java
@@ -0,0 +1,16 @@
+package me.groot314.AdvancedM;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class AdvancedM extends JavaPlugin{
+	@Override
+	public void onEnable() {
+		// TODO Auto-generated method stub
+		super.onEnable();
+	}
+	@Override
+	public void onDisable() {
+		// TODO Auto-generated method stub
+		super.onDisable();
+	}
+}
